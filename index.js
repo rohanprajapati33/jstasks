527ff237b2cde837e5c29ffb6de013927df315e0
Rohan Prajapati <190770107222@socet.edu.in>
2023-04-04 17:47:25 +0530

    implement area calculate functions

---
+++ index.js
@@ -0,0 +1,23 @@
+//area of square
+function areasquare(side) {
+  let area = side * side;
+  return area;
+}
+
+console.log("Area = " + areasquare(4));
+
+//area of rectangle
+function areaRectangle(a, b) {
+  let area = a * b;
+  return area;
+}
+
+console.log("Area = " + areaRectangle(5, 6));
+
+//area of tringle
+function areatringle(k, l, m) {
+  let area = k * l * m;
+  return area;
+}
+
+console.log("Area =" + areatringle(15, 20, 25));
